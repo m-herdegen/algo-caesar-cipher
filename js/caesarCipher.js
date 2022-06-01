28dc5b3bf9a8cbc42c58bd3190c8bb6a0167c16a
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-01 17:14:25 -0400

    also a bit wordy but it works. find something more elegant

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,75 @@
-exports.caesarCipher = function() {
+exports.caesarCipher = function(inputString, shiftAmount) {
+    let ans = ''
+    let temp = ''
+    const special = "/[!@#$%^&*()1234567890 .]/"
 
+    if(shiftAmount > 0){
+        for (i = 0; i < inputString.length; i++){
+            if (special.indexOf(inputString[i]) > -1){
+                ans += inputString[i]
+            }
+            else if(inputString[i] == inputString[i].toUpperCase()){
+                temp = inputString[i]
+                for(j = 0; j < shiftAmount; j++){
+                    temp = String.fromCharCode((temp.charCodeAt(0) + 1))
+                    if (temp.charCodeAt(0) > 90){
+                        temp = String.fromCharCode(65)
+                    }
+                }
+                ans += temp
+            }
+            else{
+                temp = inputString[i]
+                for(j = 0; j < shiftAmount; j++){
+                    
+                    temp = String.fromCharCode((temp.charCodeAt(0) + 1))
+                    if (temp.charCodeAt(0) > 122){
+                        temp = String.fromCharCode(97)
+                    }
+                }
+                ans += temp
+            }
+            
+        }
+        console.log(ans)
+        return ans
+    }
+
+    else if (shiftAmount < 0){
+        for (i = 0; i < inputString.length; i++){
+            if (inputString[i] == ' ' || special.indexOf(inputString[i]) > -1){
+                ans += inputString[i]
+            }
+            else if(inputString[i] == inputString[i].toUpperCase()){
+                temp = inputString[i]
+                for(j = 0; j > shiftAmount; j--){
+                    temp = String.fromCharCode((temp.charCodeAt(0) - 1))
+                    if (temp.charCodeAt(0) < 65){
+                        temp = String.fromCharCode(90)
+                    }
+                    
+                }
+                ans += temp
+            }
+            else{
+                temp = inputString[i]
+                for(j = 0; j > shiftAmount; j--){
+                    temp = String.fromCharCode((temp.charCodeAt(0) - 1))
+                    if (temp.charCodeAt(0) < 97){
+                        temp = String.fromCharCode(122)
+                    }
+
+                }
+                ans += temp
+            } 
+        }
+        console.log(ans)
+        return ans
+    }
+
+    else{
+        console.log(inputString)
+        return inputString
+    }
+    
 };
