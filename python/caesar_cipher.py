eec72e60c37053f98a98f11caaeb62a828e7e568
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-01 16:23:37 -0400

    technically works but lots of room to clean it up

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,48 @@
-def caesar_cipher(string, shift_amount):
+import string
+def caesar_cipher(input_string, shift_amount):
+    ans = ''
+    special = set(string.punctuation)
+
+    if shift_amount > 0:
+
+        for char in input_string:
+            if (char in special) or (char.isdigit()) or char == ' ':
+                ans += char 
+            
+            elif char.isupper():
+                for i in range(0, shift_amount, 1):
+                    char = chr(ord(char) + 1)
+                    if ord(char) > 90:
+                        char = chr(65)
+                ans += char 
+
+            elif char.islower():
+                for i in range (0, shift_amount, 1):
+                    char = chr(ord(char) + 1)
+                    if ord(char) > 122:
+                        char = chr(97)
+                ans += char 
+
+    elif shift_amount < 0:
+        for char in input_string: 
+            if (char in special) or (char.isdigit()) or char == ' ':
+                ans += char  
+            elif char.isupper():
+                for i in range(0, shift_amount, -1):
+                    char = chr(ord(char) - 1)
+
+                    if ord(char) < 65:
+                        char = chr(90)
+                ans += char 
+
+            elif char.islower():
+                for i in range (0, shift_amount, -1):
+                    char = chr(ord(char) - 1)
+
+                    if ord(char) < 97:
+                        char = chr(122)
+                ans += char         
+    else:
+        ans = input_string
+    print(ans)
+    return ans
